9d700b54f6b1482b1d3d3417f87a9ccd1a9a653f
MarianaDMG05 <mariana.gomes87@etec.sp.gov.br>
2024-09-17 20:36:38 -0300

    editado os usuarios

---
+++ AppFlatList/data/Usuario.js
@@ -0,0 +1,35 @@
+// Example of data
+export default [
+    {
+        id: '1',
+        nome: 'Mickey Mouse',
+        email: 'mickeymouse123@gmail.com',
+        urlImagem: 'https://upload.wikimedia.org/wikipedia/pt/d/d4/Mickey_Mouse.png'
+    },
+    
+    {
+        id: '2',
+        nome: 'Pato Donald',
+        email: 'patodonald567@gmail.com',
+        urlImagem: 'https://upload.wikimedia.org/wikipedia/pt/6/6f/Donald.png'
+    },
+    {
+        id: '3',
+        nome: 'Minnie Mouse',
+        email: 'minniemouse@gmail.com',
+        urlImagem: 'https://static.wikia.nocookie.net/disney/images/6/67/Minnie_Mouse.png/revision/latest?cb=20240611033558'
+    },
+    {
+        id: '4',
+        nome: 'Margarida Pato',
+        email: 'margaridapato@hotmal.com',
+        urlImagem: 'https://static.wikia.nocookie.net/disney/images/4/43/Daisydisney.jpg/revision/latest?cb=20150518021733&path-prefix=pt-br'
+    },
+    {
+        id: '5',
+        nome: 'Pluto',
+        email: 'pluto@gmail.com',
+        urlImagem: 'https://upload.wikimedia.org/wikipedia/pt/4/4c/Pluto_%28Disney%29.jpg'
+    }
+
+];
